e98dc742bd69c251f9c75cc578679531490b8f60
duleepalwis <duleepalwis@gmail.com>
2021-06-23 19:33:34 +0530

    Added heap dump reader

--- LogMetricsAnalysis/src/Application/Main.java
+++ LogMetricsAnalysis/src/Application/Main.java
@@ -38,12 +38,7 @@ public class Main {
 		}
 		
 
-		System.out.print("Enter file path : ");
-		Scanner sca = new Scanner(System.in);
-		//readBinToText(sca.nextLine());
-		ThreadDumpRead thr = new ThreadDumpRead(sca.nextLine());
 		
-		thr.readFile();
 	}
 	
 	public static void readBinToText(String file) {
